a13402834365b25f5410316a31e5970eb91292a5
unknown <kstar67453@gmail.com>
2020-11-17 17:12:06 -0500

    Create basic outline of shooter subsystem and commands

--- src/main/kotlin/com/team4099/robot2021/commands/ShootCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/ShootCommand.kt
@@ -1,4 +1,25 @@
 package com.team4099.robot2021.commands
 
-class ShootCommand {
+import com.team4099.lib.units.inRotationsPerMinute
+import com.team4099.robot2021.config.Constants
+import com.team4099.robot2021.subsystems.Shooter
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class ShootCommand : CommandBase() {
+  init{
+    addRequirements(Shooter)
+  }
+
+  override fun initialize(){
+    Shooter.setVelocity(Constants.Shooter.TARGET_VELOCITY)
+  }
+  override fun execute(){
+
+  }
+  override fun isFinished():Boolean{
+    // don't actually do this
+    return Shooter.getVelocity()!=Constants.Shooter.TARGET_VELOCITY
+  }
+
+
 }
