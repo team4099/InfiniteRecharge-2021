f2b3c26cfa4a4e6c0f84106ff2236a8fa563b462
unknown <kstar67453@gmail.com>
2020-11-17 17:11:52 -0500

    Create basic outline of shooter subsystem and commands

---
+++ src/main/kotlin/com/team4099/robot2021/commands/ShootCommand.kt
@@ -0,0 +1,4 @@
+package com.team4099.robot2021.commands
+
+class ShootCommand {
+}
---
+++ src/main/kotlin/com/team4099/robot2021/commands/ShooterIdleCommand.kt
@@ -0,0 +1,21 @@
+package com.team4099.robot2021.commands
+
+import com.team4099.robot2021.subsystems.Shooter
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class ShooterIdle : CommandBase() {
+  init{
+    addRequirements(Shooter)
+  }
+
+  override fun initialize(){
+
+  }
+  override fun execute(){
+
+  }
+  override fun isFinished(){
+
+  }
+
+}
