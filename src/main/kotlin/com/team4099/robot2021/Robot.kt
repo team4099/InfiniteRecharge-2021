96d1fb61ebb4fdfd9296406d486d3a4e28753b6c
Hazel Brown <58481586+Hazel-Brown@users.noreply.github.com>
2020-11-19 19:33:55 -0500

    Resolved dislocated ClimberPosition references

--- src/main/kotlin/com/team4099/robot2021/Robot.kt
+++ src/main/kotlin/com/team4099/robot2021/Robot.kt
@@ -1,6 +1,7 @@
 package com.team4099.robot2021
 
 import com.team4099.robot2021.commands.MoveClimber
+import com.team4099.robot2021.config.Constants
 import com.team4099.robot2021.config.ControlBoard
 import com.team4099.robot2021.subsystems.Climber
 import edu.wpi.first.wpilibj.TimedRobot
@@ -8,9 +9,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler
 
 object Robot: TimedRobot() {
   init {
-    Climber.defaultCommand = MoveClimber(Climber.ClimberPosition.LOW)//change this to set the default position as pneumatic pushed out and motor stopped
-    ControlBoard.climberHigh.whileActiveOnce(MoveClimber(Climber.ClimberPosition.HIGH))
-    ControlBoard.climberLow.whileActiveOnce(MoveClimber(Climber.ClimberPosition.LOW))
+    Climber.defaultCommand = MoveClimber(Constants.ClimberPosition.LOW)//change this to set the default position as pneumatic pushed out and motor stopped
+    ControlBoard.climberHigh.whileActiveOnce(MoveClimber(Constants.ClimberPosition.HIGH))
+    ControlBoard.climberLow.whileActiveOnce(MoveClimber(Constants.ClimberPosition.LOW))
   }
   override fun robotPeriodic() {
     CommandScheduler.getInstance().run()
