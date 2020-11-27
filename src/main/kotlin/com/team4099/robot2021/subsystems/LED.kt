4e9a95a450831efc7a7e6e01c2342e989b3f2126
Eckart Schneider <eckshn@gmail.com>
2020-11-27 17:21:13 -0500

    Add rainbow method

--- src/main/kotlin/com/team4099/robot2021/subsystems/LED.kt
+++ src/main/kotlin/com/team4099/robot2021/subsystems/LED.kt
@@ -10,13 +10,31 @@ object LED: SubsystemBase() {
 
   private val led = AddressableLED(Constants.LED.PORT)
   private val ledBuffer = AddressableLEDBuffer(Constants.LED.LED_COUNT)
+  private var rainbowFirstPixelHue = 0
+
   init {
     led.setLength(ledBuffer.length)
     led.setData(ledBuffer)
     led.start()
+
   }
 
   override fun periodic() {
 
+  }
+
+  private fun rainbow() {
+    // For every pixel
+    for (i in 0 until ledBuffer.getLength()) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      val hue = (rainbowFirstPixelHue + i * 180 / ledBuffer.getLength()) % 180
+      // Set the value
+      ledBuffer.setHSV(i, hue, 255, 128)
+    }
+    // Increase by to make the rainbow "move"
+    rainbowFirstPixelHue += 3
+    // Check bounds
+    rainbowFirstPixelHue %= 180
   }
 }
