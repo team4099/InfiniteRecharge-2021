36be9b9b178e925b2224450d669485cb6ddfc7d4
Eckart Schneider <eckshn@gmail.com>
2020-11-27 16:37:27 -0500

    current progress (need to switch branch)

--- src/main/kotlin/com/team4099/robot2021/subsystems/LED.kt
+++ src/main/kotlin/com/team4099/robot2021/subsystems/LED.kt
@@ -6,6 +6,8 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 object LED: SubsystemBase() {
+  //using https://www.amazon.com/ALITOVE-Individually-Addressable-Flexible-Waterproof/dp/B018X04ES2
+
   private val led = AddressableLED(Constants.LED.PORT)
   private val ledBuffer = AddressableLEDBuffer(Constants.LED.LED_COUNT)
   init {
