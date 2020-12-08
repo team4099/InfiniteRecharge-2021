1c40cc33b2e21565ebd18c0be550249ee9fde5aa
Kandasamy Chokkalingam <samchoks@hotmail.com>
2020-12-08 17:50:16 -0500

    Move loggers to each wheel and add more motor specific values to be logged

--- src/main/kotlin/com/team4099/robot2021/subsystems/Wheel.kt
+++ src/main/kotlin/com/team4099/robot2021/subsystems/Wheel.kt
@@ -4,6 +4,7 @@ import com.ctre.phoenix.sensors.CANCoder
 import com.revrobotics.CANSparkMax
 import com.revrobotics.ControlType
 import com.revrobotics.SparkMax
+import com.team4099.lib.logging.Logger
 import com.team4099.lib.units.*
 import com.team4099.lib.units.base.feet
 import com.team4099.lib.units.base.inches
@@ -11,7 +12,7 @@ import com.team4099.lib.units.derived.*
 import com.team4099.robot2021.config.Constants
 import kotlin.math.IEEErem
 
-class Wheel(private val directionSpark: CANSparkMax, private val driveSpark: CANSparkMax,  private val encoder: CANCoder, private val zeroOffset: Angle) {
+class Wheel(private val directionSpark: CANSparkMax, private val driveSpark: CANSparkMax,  private val encoder: CANCoder, private val zeroOffset: Angle, public val label: String) {
 
   private val directionPID = directionSpark.pidController
   private val drivePID = driveSpark.pidController
@@ -20,6 +21,32 @@ class Wheel(private val directionSpark: CANSparkMax, private val driveSpark: CAN
   private val driveSensor = sparkMaxLinearMechanismSensor(driveSpark, 1.0, 3.inches)
 
   private val directionAbsolute = AngularMechanismSensor(1.0,Timescale.CTRE,{encoder.velocity},{encoder.absolutePosition})
+
+  // motor params
+  val driveTemp: Double
+    get() = driveSpark.getMotorTemperature()
+
+  val directionTemp: Double
+    get() = directionSpark.getMotorTemperature()
+
+  val driveOutputCurrent: Double
+    get() = directionSpark.getOutputCurrent()
+
+  val directionOutputCurrent: Double
+    get() = directionSpark.getOutputCurrent()
+
+  val drivePercentOutput: Double
+    get() = driveSpark.get()
+
+  val directionPercentOutput: Double
+    get() = directionSpark.get()
+
+  val driveBusVoltage: Double
+    get() = driveSpark.getBusVoltage()
+
+  val directionBusVoltage: Double
+    get() = directionSpark.getBusVoltage()
+
 
   private var speedSetPoint: LinearVelocity = 0.feet.perSecond
     set(value) {
@@ -31,6 +58,21 @@ class Wheel(private val directionSpark: CANSparkMax, private val driveSpark: CAN
       directionPID.setReference(directionSensor.positionToRawUnits(value), ControlType.kSmartMotion)
       field = value
     }
+
+  init {
+    Logger.addSource("Drivetrain", label + " Drive Output Current") { driveOutputCurrent}
+    Logger.addSource("Drivetrain", label + " Direction Output Current") { directionOutputCurrent }
+
+    Logger.addSource("Drivetrain", label + " Drive Temperature") { driveTemp }
+    Logger.addSource("Drivetrain", label + " Direction Temperature") { directionTemp }
+
+    Logger.addSource("Drivetrain", label + " Drive Percent Output") { drivePercentOutput }
+    Logger.addSource("Drivetrain", label + " Direction Percent Output") { directionPercentOutput }
+
+    Logger.addSource("Drivetrain", label + " Drive Bus Voltage") { driveBusVoltage }
+    Logger.addSource("Drivetrain", label + " Direction Bus Voltage") { directionBusVoltage }
+
+  }
 
   fun set(direction: Angle, speed: LinearVelocity) {
     if(speed == 0.feet.perSecond){
