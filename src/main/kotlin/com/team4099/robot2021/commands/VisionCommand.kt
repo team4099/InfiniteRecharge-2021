ae8ad811af1e213d1c706d6e0e5e7e27803836ab
unknown <kstar67453@gmail.com>
2020-12-08 17:55:22 -0500

    Move PID controller to vision command

--- src/main/kotlin/com/team4099/robot2021/commands/VisionCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/VisionCommand.kt
@@ -5,10 +5,23 @@ import com.team4099.lib.units.derived.inDegrees
 import com.team4099.lib.units.derived.tan
 import com.team4099.robot2021.config.Constants
 import com.team4099.robot2021.subsystems.Vision
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile
 import edu.wpi.first.wpilibj2.command.CommandBase
 import kotlin.math.sign
 
 class VisionCommand : CommandBase(){
+
+  val visionPIDcontroller = ProfiledPIDController(
+    Constants.Vision.TurnGains.KP,
+    Constants.Vision.TurnGains.KI,
+    Constants.Vision.TurnGains.KD,
+    TrapezoidProfile.Constraints(
+      Constants.Vision.TurnGains.MAX_VELOCITY.value,
+      Constants.Vision.TurnGains.MAX_ACCEL.value
+    )
+  )
+
   init {
     addRequirements(Vision)
   }
@@ -18,15 +31,10 @@ class VisionCommand : CommandBase(){
     Logger.addEvent("VisionCommand","Started vision command")
   }
   override fun execute() {
-    /*Vision.steeringAdjust = when{
-      Vision.onTarget -> Vision.onTargetController.calculate(Vision.tx.inDegrees, 0.0)
-      else -> Vision.offTargetController.calculate(Vision.tx.inDegrees, 0.0)
-    }*/
     if(Vision.tv != 0.0){
-      Vision.steeringAdjust = Vision.visionPIDcontroller.calculate(Vision.tx.value, 0.0)
+      Vision.steeringAdjust = visionPIDcontroller.calculate(Vision.tx.value, 0.0)
       Vision.steeringAdjust += sign(Vision.tx.value) * Constants.Vision.MIN_TURN_COMMAND
     }
-
   }
   override fun isFinished() : Boolean {
     return false
