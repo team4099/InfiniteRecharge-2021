8f0ff47caaf7f2eb52fb5f2d08f054409c8e3419
Eckart Schneider <eckshn@gmail.com>
2020-12-15 17:32:11 -0500

    failure manager add methods and enums

--- src/main/kotlin/com/team4099/robot2021/loops/FailureManager.kt
+++ src/main/kotlin/com/team4099/robot2021/loops/FailureManager.kt
@@ -1,27 +1,35 @@
 package com.team4099.robot2021.loops
 
 import com.team4099.lib.logging.Logger
+import org.graalvm.compiler.core.common.cfg.Loop
 
-object FailureManager : Loop {
-  var rio3v3Faults = 0
-    set(faults) {
-      if (field != faults) {
-        Logger.addEvent("Fault Detector", "roboRIO 3.3V Fault")
-      }
-      field = faults
+object FailureManager {
+
+  enum class Severity {
+    WARNING,
+    ERROR
+  }
+
+  enum class Failures (val severity : Severity) {
+    PRESSURE_LEAK(Severity.ERROR)
+  }
+
+  //have the motor as a parameter so it can be controlled (eg. motor drawing too much current so shut it down)
+  fun addFailure(failType: Failures, condition: () -> Boolean) {
+    //what to do with error?
+    when(failType) {
+
     }
-  var rio5vFaults = 0
-    set(faults) {
-      if (field != faults) {
-        Logger.addEvent("Fault Detector", "roboRIO 5V Fault")
-      }
-      field = faults
-    }
-  var rio6vFaults = 0
-    set(faults) {
-      if (field != faults) {
-        Logger.addEvent("Fault Detector", "roboRIO 6V Fault")
-      }
-      field = faults
-    }
+  }
+
+  //fun removeFailure?
+
+  fun addTestFailure(failType: Failures, condition: () -> Boolean) {
+
+  }
+
+
+
+
+
 }
