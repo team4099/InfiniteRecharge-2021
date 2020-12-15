f951341f471b83812079000194dc47ebdb50f86c
flash76 <30060599+flash76@users.noreply.github.com>
2020-12-15 17:50:44 -0500

    Implement PathStore from 2020 code

---
+++ src/main/kotlin/com/team4099/robot2021/commands/drivetrain/AutoDriveCommand.kt
@@ -0,0 +1,8 @@
+package com.team4099.robot2021.commands.drivetrain
+
+import edu.wpi.first.wpilibj2.command.CommandBase
+import com.team4099.robot2021.subsystems.Drivetrain
+
+class AutoDriveCommand : CommandBase() {
+
+}
