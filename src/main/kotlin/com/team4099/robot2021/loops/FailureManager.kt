cac8d438a7b4ec2ac132725adbc1c4eea66d4649
MadManV <48395615+MadManV@users.noreply.github.com>
2020-12-16 19:19:43 -0500

    created a flag map for the failures.

--- src/main/kotlin/com/team4099/robot2021/loops/FailureManager.kt
+++ src/main/kotlin/com/team4099/robot2021/loops/FailureManager.kt
@@ -5,21 +5,25 @@ import org.graalvm.compiler.core.common.cfg.Loop
 
 object FailureManager {
 
+
+  //Map of Error Flags
+  private val errorFlags = mutableMapOf(Failures.PRESSURE_LEAK to false, Failures.INTAKE_SPEC_VIOLATION to false)
+
   enum class Severity {
     WARNING,
     ERROR
   }
 
   enum class Failures (val severity : Severity) {
-    PRESSURE_LEAK(Severity.ERROR)
+    PRESSURE_LEAK(Severity.ERROR),
+    INTAKE_SPEC_VIOLATION(Severity.WARNING)
   }
 
   //have the motor as a parameter so it can be controlled (eg. motor drawing too much current so shut it down)
+  //Create a when (switch) statement in the teleopInit for the loop
   fun addFailure(failType: Failures, condition: () -> Boolean) {
-    //what to do with error?
-    when(failType) {
-
-    }
+    //Set the failure to true
+    errorFlags[failType] = true;
   }
 
   //fun removeFailure?
