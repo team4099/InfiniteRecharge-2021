f507510c7952da91dbef7d35a86bd4a719ad90c5
Kandasamy Chokkalingam <samchoks@hotmail.com>
2021-01-12 18:22:58 -0500

    Fix import errors in EightBallMode

--- src/main/kotlin/com/team4099/robot2021/auto/modes/EightBallMode.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/modes/EightBallMode.kt
@@ -2,7 +2,10 @@ package com.team4099.robot2021.auto.modes
 
 import com.team4099.robot2021.auto.PathStore
 import com.team4099.robot2021.commands.drivetrain.AutoDriveCommand
+import com.team4099.robot2021.commands.intake.IntakeCommand
+import com.team4099.robot2021.config.Constants
 import com.team4099.robot2021.subsystems.Drivetrain
+import com.team4099.robot2021.subsystems.Intake
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 
@@ -14,7 +17,7 @@ class EightBallMode : SequentialCommandGroup() {
     addCommands(
       ParallelRaceGroup(
         AutoDriveCommand(PathStore.toRendezvousPoint2Balls),
-        IntakeCommand(IntakeState.IN)
+        IntakeCommand(Constants.Intake.IntakeState.IN,Constants.Intake.ArmPosition.OUT)
       ),
 
       AutoDriveCommand(PathStore.fromRendezvousPoint2Balls),
@@ -23,7 +26,7 @@ class EightBallMode : SequentialCommandGroup() {
 
       ParallelRaceGroup(
         SequentialCommandGroup(AutoDriveCommand(PathStore.toNearTrench),AutoDriveCommand(PathStore.intakeInNearTrench)),
-        IntakeCommand()
+        IntakeCommand(Constants.Intake.IntakeState.IN,Constants.Intake.ArmPosition.IN)
       ),
 
       AutoDriveCommand(PathStore.fromRendezvousPoint2Balls),
