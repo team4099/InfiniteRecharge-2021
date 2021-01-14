4fce12833489702ea0b7e59b4481a4a60bb6e7b5
Kandasamy Chokkalingam <samchoks@hotmail.com>
2021-01-14 17:26:20 -0500

    Added DriveCharacterizeCommand and used Clock for time

--- src/main/kotlin/com/team4099/robot2021/auto/modes/FarTrenchFiveBallMode.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/modes/FarTrenchFiveBallMode.kt
@@ -6,8 +6,12 @@ import com.team4099.robot2021.commands.intake.IntakeCommand
 import com.team4099.robot2021.config.Constants
 import com.team4099.robot2021.subsystems.Drivetrain
 import com.team4099.robot2021.subsystems.Intake
+import com.team4099.lib.units.base.seconds
+
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+
 
 class FarTrenchFiveBallMode : SequentialCommandGroup(){
 
@@ -16,7 +20,7 @@ class FarTrenchFiveBallMode : SequentialCommandGroup(){
 
     addCommands(
       ParallelRaceGroup(
-        SequentialCommandGroup(AutoDriveCommand(PathStore.toFarTrench), WaitAction(0.5)),
+        SequentialCommandGroup(AutoDriveCommand(PathStore.toFarTrench), WaitCommand(0.5)),
         IntakeCommand(Constants.Intake.IntakeState.IN, Constants.Intake.ArmPosition.OUT) //states may be wrong
       ),
       AutoDriveCommand(PathStore.fromFarTrench),
