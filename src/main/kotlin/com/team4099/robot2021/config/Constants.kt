d8bc0138397c75fd516b7de12a2fcbe3c936b89a
Aidan Goettsch <aidangoettsch@gmail.com>
2021-01-14 17:42:04 -0500

    undo random deletes

--- src/main/kotlin/com/team4099/robot2021/config/Constants.kt
+++ src/main/kotlin/com/team4099/robot2021/config/Constants.kt
@@ -11,6 +11,23 @@ object Constants {
     const val EPSILON = 1E-9
   }
 
+  object Tuning {
+    const val TUNING_TOGGLE_PIN = 0
+    val ROBOT_ID_PINS = 1..2
+
+    enum class RobotName {
+      COMPETITION,
+      PRACTICE,
+      MULE
+    }
+
+    val ROBOT_ID_MAP = mapOf<Int, RobotName>(
+      0 to RobotName.COMPETITION,
+      1 to RobotName.PRACTICE,
+      2 to RobotName.MULE
+    )
+  }
+
   object Joysticks {
     const val DRIVER_PORT = 0
     const val SHOTGUN_PORT = 1
