2ecf1b3317906d6dc7b80156b820a5d30bb3f386
Eckart Schneider <eckshn@gmail.com>
2021-02-04 17:35:29 -0500

    Fix shoot all again

--- src/main/kotlin/com/team4099/robot2021/commands/shooter/ShootAllCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/shooter/ShootAllCommand.kt
@@ -3,17 +3,18 @@ package com.team4099.robot2021.commands.shooter
 import com.team4099.robot2021.commands.feeder.WaitUntilCommand
 import com.team4099.robot2021.subsystems.Shooter
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 
-class ShootAllCommand : SequentialCommandGroup() {
+class ShootAllCommand : ParallelRaceGroup() {
   init{
     addRequirements(Shooter)
 
-    ParallelRaceGroup(
-      WaitCommand(5.0), //subject to change
-      WaitUntilCommand(0),
-      ShootCommand()
+    addCommands(
+      ParallelRaceGroup(
+        WaitCommand(5.0), //subject to change
+        WaitUntilCommand(0),
+        ShootCommand()
+      )
     )
   }
 }
