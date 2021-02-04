f72b246af71f0de3a926828d42df88b4ee5d8a03
Eckart Schneider <eckshn@gmail.com>
2021-02-04 17:32:43 -0500

    Fix ShootAllCommand

--- src/main/kotlin/com/team4099/robot2021/commands/shooter/ShootAllCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/shooter/ShootAllCommand.kt
@@ -1,17 +1,19 @@
 package com.team4099.robot2021.commands.shooter
 
-import com.team4099.robot2021.subsystems.Feeder
+import com.team4099.robot2021.commands.feeder.WaitUntilCommand
 import com.team4099.robot2021.subsystems.Shooter
-import edu.wpi.first.wpilibj2.command.CommandBase
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
 
-class ShootAllCommand : CommandBase() {
-  init {
-      addRequirements(Shooter, Feeder)
-  }
+class ShootAllCommand : SequentialCommandGroup() {
+  init{
+    addRequirements(Shooter)
 
-  override fun initialize() {
-    while(Feeder.ballCount > 0) {
+    ParallelRaceGroup(
+      WaitCommand(5.0), //subject to change
+      WaitUntilCommand(0),
       ShootCommand()
-    }
+    )
   }
 }
