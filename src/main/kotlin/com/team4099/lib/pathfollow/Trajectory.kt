7d7d27c5b5c03776e8358f0d215666b060e62ae2
Aidan Goettsch <aidangoettsch@gmail.com>
2021-02-09 17:36:09 -0500

    initial trajectory generator work

--- src/main/kotlin/com/team4099/lib/pathfollow/Trajectory.kt
+++ src/main/kotlin/com/team4099/lib/pathfollow/Trajectory.kt
@@ -3,15 +3,36 @@ package com.team4099.lib.pathfollow
 import com.team4099.lib.geometry.Pose
 import com.team4099.lib.geometry.Translation
 import com.team4099.lib.units.LinearVelocity
+import com.team4099.lib.units.inMetersPerSecond
+import com.team4099.lib.units.inMetersPerSecondPerSecond
+import edu.wpi.first.wpilibj.spline.PoseWithCurvature
+import edu.wpi.first.wpilibj.spline.Spline
+import edu.wpi.first.wpilibj.spline.SplineParameterizer
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
+import edu.wpi.first.wpilibj.trajectory.TrajectoryParameterizer
+import java.util.ArrayList
 
+/**
+ * A wrapper around the WPILib trajectory class that handles smooth
+ * heading changes for holonomic drivetrains.
+ */
 class Trajectory(
-  private val startPose: Pose,
   private val startVelocity: LinearVelocity,
-  guidePoints: List<Translation> = listOf(),
-  private val endPose: Pose,
-  private val endVelocity: LinearVelocity
+  private val path: Path,
+  private val endVelocity: LinearVelocity,
+  private val trajectoryConfig: TrajectoryConfig
 ) {
   init {
-    
+    val states = TrajectoryParameterizer.timeParameterizeTrajectory(
+      path.splinePoints,
+      trajectoryConfig.constraints,
+      startVelocity.inMetersPerSecond,
+      endVelocity.inMetersPerSecond,
+      trajectoryConfig.maxLinearVelocity.inMetersPerSecond,
+      trajectoryConfig.maxLinearAcceleration.inMetersPerSecondPerSecond,
+      false
+    ).states
+
+    val headingTargets =
   }
 }
---
+++ src/main/kotlin/com/team4099/lib/pathfollow/TrajectoryState.kt
@@ -0,0 +1,17 @@
+package com.team4099.lib.pathfollow
+
+import com.team4099.lib.geometry.Pose
+import com.team4099.lib.units.AngularAcceleration
+import com.team4099.lib.units.AngularVelocity
+import com.team4099.lib.units.LinearAcceleration
+import com.team4099.lib.units.LinearVelocity
+import com.team4099.lib.units.base.Time
+
+data class TrajectoryState(
+  val timestamp: Time,
+  var pose: Pose,
+  val linearVelocity: LinearVelocity,
+  val linearAcceleration: LinearAcceleration,
+  var angularVelocity: AngularVelocity,
+  var angularAcceleration: AngularAcceleration
+)
