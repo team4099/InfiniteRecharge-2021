5177a4c09bd1e4ff46a814aa4cde10a4893ba89d
Aidan Goettsch <aidangoettsch@gmail.com>
2021-02-10 20:00:31 -0500

    update logger to support config from shuffleboard and add kdoc

--- src/main/kotlin/com/team4099/lib/logging/Logger.kt
+++ src/main/kotlin/com/team4099/lib/logging/Logger.kt
@@ -1,27 +1,35 @@
 package com.team4099.lib.logging
 
+import edu.wpi.first.networktables.EntryListenerFlags
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Notifier
 import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import java.io.File
 import java.io.IOException
+import java.lang.ClassCastException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 import java.time.Instant
 
+/**
+ * The global logger object.
+ *
+ * Manages Shuffleboard entries and CSV logging of values.
+ */
 object Logger {
   private val dataSources = mutableListOf<LogSource>()
   private val logNotifier = Notifier(this::saveEvents)
   private lateinit var file: Path
   private lateinit var eventsFile: Path
   private var loggingLocation: String =
-      when {
+    when {
         RobotBase.isSimulation() -> "./logs"
         File("/media/sda1/").exists() -> "/media/sda1/logs/"
         else -> "/home/lvuser/logs/"
-      }
+    }
 
   private var values: String = ""
     get() {
@@ -30,6 +38,9 @@ object Logger {
     }
   private val events = mutableListOf<String>()
 
+  /**
+   * Severity of an event.
+   */
   enum class Severity {
     INFO,
     DEBUG,
@@ -58,14 +69,14 @@ object Logger {
             Paths.get("${loggingLocation}test.csv")
           }
       eventsFile =
-          if (DriverStation.getInstance().isFMSAttached) {
-            Paths.get(
-                "$loggingLocation${DriverStation.getInstance().eventName}_" +
-                    "${DriverStation.getInstance().matchType}" +
-                    "${DriverStation.getInstance().matchNumber}Events.csv")
-          } else {
-            Paths.get("${loggingLocation}testEvents.csv")
-          }
+        if (DriverStation.getInstance().isFMSAttached) {
+          Paths.get(
+            "$loggingLocation${DriverStation.getInstance().eventName}_" +
+              "${DriverStation.getInstance().matchType}" +
+              "${DriverStation.getInstance().matchNumber}Events.csv")
+        } else {
+          Paths.get("${loggingLocation}testEvents.csv")
+        }
 
       if (Files.exists(file)) Files.delete(file)
       Files.createFile(file)
@@ -79,10 +90,40 @@ object Logger {
     }
   }
 
-  fun <T> addSource(tab: String, name: String, supplier: () -> T) {
+  /**
+   * Add a source of data for the logger.
+   *
+   * @param tab The name of the Shuffleboard tab to add this value to. Typically the
+   * subsystem name.
+   * @param name The name of this value.
+   * @param supplier A function which returns the value to be logged.
+   * @param setter An optional function which will be called when the value in
+   * Shuffleboard is changed.
+   */
+  fun <T> addSource(tab: String, name: String, supplier: () -> T, setter: ((T) -> Unit)? = null) {
     dataSources.add(LogSource(tab, name) { supplier().toString() })
+    val shuffleboardEntry = Shuffleboard.getTab(tab).add(name, supplier)
+
+    if (setter != null) {
+      shuffleboardEntry.entry.addListener({
+        val newValue = it.getEntry().value
+
+        try {
+          // Unchecked cast since we don't know the type of this
+          // source due to type erasure
+          @Suppress("UNCHECKED_CAST")
+          setter(newValue as T)
+        } catch (e: ClassCastException) {
+          addEvent("Logger", "Could not change value for $tab/$name due to" +
+            "invalid type cast.", Severity.ERROR)
+        }
+      }, EntryListenerFlags.kUpdate)
+    }
   }
 
+  /**
+   * Write logs to the CSV file.
+   */
   fun saveLogs() {
     try {
       val data = "${Instant.now()},${DriverStation.getInstance().matchTime},$values"
@@ -92,6 +133,9 @@ object Logger {
     }
   }
 
+  /**
+   * Begin logging. Creates CSV files and starts the logging thread.
+   */
   fun startLogging() {
     createFile()
     logNotifier.startPeriodic(1.0)
@@ -103,17 +147,24 @@ object Logger {
     Files.write(file, listOf(titles), StandardOpenOption.APPEND)
   }
 
+  /**
+   * Track an event that occurs. Saves the event to a CSV and prints it to the console.
+   *
+   * @param source The source of the event, typically a subsystem or class name.
+   * @param event The text to log.
+   * @param severity The severity of the event. Defaults to INFO. Events with severity
+   * ERROR will be logged to stderr instead of stdout.
+   */
   fun addEvent(source: String, event: String, severity: Severity = Severity.INFO) {
-    val log =
-        "$severity,${Instant.now()},${DriverStation.getInstance().matchTime}," + "($source),$event"
+    val log = "$severity,${Instant.now()},${DriverStation.getInstance().matchTime}," +
+      "($source),$event"
     events.add(log)
-    val consoleString =
-        "[$severity][${Instant.now()}][${DriverStation.getInstance().matchTime}] " +
-            "($source): $event"
+    val consoleString = "[$severity][${Instant.now()}][${DriverStation.getInstance().matchTime}] " +
+      "($source): $event"
     when (severity) {
       Severity.INFO -> println(consoleString)
       Severity.DEBUG -> println(consoleString)
-      Severity.ERROR -> error(consoleString)
+      Severity.ERROR -> System.err.println(consoleString)
     }
   }
 
