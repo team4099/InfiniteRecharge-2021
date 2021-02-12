93010638ebad6d547201f1f6b7849790d740c7d0
RehanKabir <rehankabir10@gmail.com>
2021-02-12 15:38:59 -0500

    correct pid

--- src/main/kotlin/com/team4099/robot2021/config/Constants.kt
+++ src/main/kotlin/com/team4099/robot2021/config/Constants.kt
@@ -44,21 +44,21 @@ object Constants {
   object Drivetrain {
     const val TICKS = 4096
 
-    const val FRONT_LEFT_SPEED_ID = 2
-    const val FRONT_LEFT_DIRECTION_ID = 3
-    const val FRONT_LEFT_CANCODER_ID = 11
+    const val FRONT_LEFT_SPEED_ID = 11
+    const val FRONT_LEFT_DIRECTION_ID = 21
+    const val FRONT_LEFT_CANCODER_ID = 1
 
-    const val FRONT_RIGHT_SPEED_ID = 4
-    const val FRONT_RIGHT_DIRECTION_ID = 5
-    const val FRONT_RIGHT_CANCODER_ID = 12
+    const val FRONT_RIGHT_SPEED_ID = 12
+    const val FRONT_RIGHT_DIRECTION_ID = 22
+    const val FRONT_RIGHT_CANCODER_ID = 2
 
-    const val BACK_LEFT_SPEED_ID = 6
-    const val BACK_LEFT_DIRECTION_ID = 7
-    const val BACK_LEFT_CANCODER_ID = 24
+    const val BACK_RIGHT_SPEED_ID = 13
+    const val BACK_RIGHT_DIRECTION_ID = 23
+    const val BACK_RIGHT_CANCODER_ID = 3
 
-    const val BACK_RIGHT_SPEED_ID = 8
-    const val BACK_RIGHT_DIRECTION_ID = 9
-    const val BACK_RIGHT_CANCODER_ID = 23
+    const val BACK_LEFT_SPEED_ID = 14
+    const val BACK_LEFT_DIRECTION_ID = 24
+    const val BACK_LEFT_CANCODER_ID = 4
 
     const val WHEEL_COUNT = 4
     val DRIVETRAIN_LENGTH = 22.173.inches
@@ -89,15 +89,15 @@ object Constants {
     }
 
     object PID {
-      const val DIRECTION_KP = 0.0
+      const val DIRECTION_KP = 0.00001
       const val DIRECTION_KI = 0.0
-      const val DIRECTION_KD = 0.0
-      const val DIRECTION_KFF = 0.0
+      const val DIRECTION_KD = 3.0
+      const val DIRECTION_KFF = 0.000078
 
-      const val DRIVE_KP = 0.00001
+      const val DRIVE_KP = 0.0
       const val DRIVE_KI = 0.0
-      const val DRIVE_KD = 3.0
-      const val DRIVE_KFF = 0.000078
+      const val DRIVE_KD = 0.0
+      const val DRIVE_KFF = 0.0
     }
   }
 
