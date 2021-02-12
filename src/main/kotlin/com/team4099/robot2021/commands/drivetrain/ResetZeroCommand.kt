533452660b980a216dc8a9c93e41ddc8d060cb30
Aidan Goettsch <aidangoettsch@gmail.com>
2021-02-12 16:07:14 -0500

    fix acceleration sensor units

--- src/main/kotlin/com/team4099/robot2021/commands/drivetrain/ResetZeroCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/drivetrain/ResetZeroCommand.kt
@@ -4,6 +4,10 @@ import com.team4099.robot2021.subsystems.Drivetrain
 import edu.wpi.first.wpilibj2.command.CommandBase
 
 class ResetZeroCommand : CommandBase() {
+  init {
+    addRequirements(Drivetrain)
+  }
+
   override fun initialize() {
     Drivetrain.resetModuleZero()
   }
