d6c27292a13acb0d5cdde1fc08e0be484edc8540
RehanKabir <rehankabir10@gmail.com>
2021-02-16 18:37:24 -0500

    commented out necessary stuff to test drivetrain

--- src/main/kotlin/com/team4099/robot2021/Robot.kt
+++ src/main/kotlin/com/team4099/robot2021/Robot.kt
@@ -4,6 +4,7 @@ import com.team4099.lib.logging.Logger
 import com.team4099.robot2021.commands.MoveClimber
 import com.team4099.robot2021.commands.climber.LockClimber
 import com.team4099.robot2021.commands.climber.UnlockClimber
+import com.team4099.robot2021.commands.drivetrain.ResetZeroCommand
 import com.team4099.robot2021.commands.feeder.FeederBeamBreak
 import com.team4099.robot2021.commands.feeder.FeederCommand
 import com.team4099.robot2021.config.Constants
@@ -37,42 +38,42 @@ object Robot : TimedRobot() {
     robotName =
         Constants.Tuning.ROBOT_ID_MAP.getOrDefault(robotId, Constants.Tuning.RobotName.COMPETITION)
     Logger.addEvent("Robot", "Robot Construction (running on $robotName)")
-    Logger.addSource("Robot", "Battery Voltage", RobotController::getBatteryVoltage)
+//    Logger.addSource("Robot", "Battery Voltage", RobotController::getBatteryVoltage)
 
     Logger.startLogging()
 
-    // Link between feeder Trigger and Command
-    Feeder.defaultCommand = FeederCommand(Feeder.FeederState.NEUTRAL)
-    ControlBoard.runFeederIn.whileActiveOnce(FeederCommand(Feeder.FeederState.FORWARD_FLOOR))
-    ControlBoard.runFeederOut.whileActiveOnce(FeederCommand(Feeder.FeederState.BACKWARD))
-
-    Intake.defaultCommand =
-        IntakeCommand(Constants.Intake.IntakeState.DEFAULT, Constants.Intake.ArmPosition.IN)
-    ControlBoard.runIntakeIn
-        .whileActiveContinuous(
-            IntakeCommand(Constants.Intake.IntakeState.IN, Constants.Intake.ArmPosition.OUT)
-                .alongWith(FeederBeamBreak()))
-    ControlBoard.runIntakeOut
-        .whileActiveContinuous(
-            IntakeCommand(Constants.Intake.IntakeState.OUT, Constants.Intake.ArmPosition.OUT)
-                .alongWith(FeederCommand(Feeder.FeederState.BACKWARD)))
-
-    Climber.defaultCommand = LockClimber()
-    ControlBoard.climberHigh
-        .whileActiveOnce(UnlockClimber().andThen(MoveClimber(Constants.ClimberPosition.HIGH)))
-    ControlBoard.climberLow
-        .whileActiveOnce(UnlockClimber().andThen(MoveClimber(Constants.ClimberPosition.LOW)))
-
-    Shooter.defaultCommand = ShooterIdleCommand()
-    ControlBoard.shoot.whenActive(ParallelCommandGroup(ShootCommand(), VisionCommand()))
-    ControlBoard.stopShooting.whenActive(ShooterIdleCommand())
+//    // Link between feeder Trigger and Command
+//    Feeder.defaultCommand = FeederCommand(Feeder.FeederState.NEUTRAL)
+//    ControlBoard.runFeederIn.whileActiveOnce(FeederCommand(Feeder.FeederState.FORWARD_FLOOR))
+//    ControlBoard.runFeederOut.whileActiveOnce(FeederCommand(Feeder.FeederState.BACKWARD))
+//
+//    Intake.defaultCommand =
+//        IntakeCommand(Constants.Intake.IntakeState.DEFAULT, Constants.Intake.ArmPosition.IN)
+//    ControlBoard.runIntakeIn
+//        .whileActiveContinuous(
+//            IntakeCommand(Constants.Intake.IntakeState.IN, Constants.Intake.ArmPosition.OUT)
+//                .alongWith(FeederBeamBreak()))
+//    ControlBoard.runIntakeOut
+//        .whileActiveContinuous(
+//            IntakeCommand(Constants.Intake.IntakeState.OUT, Constants.Intake.ArmPosition.OUT)
+//                .alongWith(FeederCommand(Feeder.FeederState.BACKWARD)))
+//
+//    Climber.defaultCommand = LockClimber()
+//    ControlBoard.climberHigh
+//        .whileActiveOnce(UnlockClimber().andThen(MoveClimber(Constants.ClimberPosition.HIGH)))
+//    ControlBoard.climberLow
+//        .whileActiveOnce(UnlockClimber().andThen(MoveClimber(Constants.ClimberPosition.LOW)))
+//
+//    Shooter.defaultCommand = ShooterIdleCommand()
+//    ControlBoard.shoot.whenActive(ParallelCommandGroup(ShootCommand(), VisionCommand()))
+//    ControlBoard.stopShooting.whenActive(ShooterIdleCommand())
 
     Drivetrain.defaultCommand = TeleopDriveCommand({ ControlBoard.strafe }, { ControlBoard.forward }, { ControlBoard.turn })
 
-    ControlBoard.spinUpShooter.whenActive(SpinUpCommand(true))
+//    ControlBoard.spinUpShooter.whenActive(SpinUpCommand(true))
   }
 
-  private val autonomousCommand = InstantCommand()
+  private val autonomousCommand = ResetZeroCommand()
 
   override fun autonomousInit() {
     autonomousCommand.schedule()
