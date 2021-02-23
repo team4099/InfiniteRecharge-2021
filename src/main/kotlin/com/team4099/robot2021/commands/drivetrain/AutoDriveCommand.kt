2225986a9851c2e868faaf67fe314236479deb98
Rithvik <rbhogavilli@gmail.com>
2021-02-23 16:53:30 -0500

    Convert trajectory in AutoDriveCommand to use our implementation

--- src/main/kotlin/com/team4099/robot2021/commands/drivetrain/AutoDriveCommand.kt
+++ src/main/kotlin/com/team4099/robot2021/commands/drivetrain/AutoDriveCommand.kt
@@ -1,13 +1,9 @@
 package com.team4099.robot2021.commands.drivetrain
 
 import com.team4099.lib.hal.Clock
-import com.team4099.lib.units.base.inSeconds
+import com.team4099.lib.pathfollow.Trajectory
 import edu.wpi.first.wpilibj2.command.CommandBase
 import com.team4099.robot2021.subsystems.Drivetrain
-import edu.wpi.first.wpilibj.Timer
-import edu.wpi.first.wpilibj.trajectory.Trajectory
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
-import edu.wpi.first.wpilibj2.command.Subsystem
 
 class AutoDriveCommand(private val path: Trajectory) : CommandBase() {
 
@@ -17,7 +13,6 @@ class AutoDriveCommand(private val path: Trajectory) : CommandBase() {
 
   override fun initialize() {
     Drivetrain.path = this.path
-    Drivetrain.isFieldOriented = false
   }
 
   override fun execute() {
