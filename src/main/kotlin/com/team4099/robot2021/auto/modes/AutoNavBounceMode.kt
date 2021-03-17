5c5d575e6a2226730812129a97cc47f4c2b5256d
unknown <kstar67453@gmail.com>
2021-03-17 13:02:39 -0400

    fix mode name

--- src/main/kotlin/com/team4099/robot2021/auto/modes/AutoNavBounceMode.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/modes/AutoNavBounceMode.kt
@@ -5,7 +5,7 @@ import com.team4099.robot2021.commands.drivetrain.AutoDriveCommand
 import com.team4099.robot2021.subsystems.Drivetrain
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 
-class AutoNavMode : SequentialCommandGroup() {
+class AutoNavBounceMode : SequentialCommandGroup() {
   init{
     addRequirements(Drivetrain)
 
