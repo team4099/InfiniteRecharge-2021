cc1605f8b739321095b988467e5abf3f9a746383
Eckart Schneider <eckshn@gmail.com>
2021-03-17 12:45:35 -0400

    Update slalom degrees

--- src/main/kotlin/com/team4099/robot2021/auto/PathStore.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/PathStore.kt
@@ -245,16 +245,17 @@ object PathStore {
 
   val slalomPath = Path(
     Pose(navPoints["E"]!![2] - Translation(15.inches, 0.feet), 0.degrees),
-    Pose(navPoints["C"]!![1], 180.degrees)
+    Pose(navPoints["C"]!![1], 0.degrees)
   ).apply {
-    addWaypoint(navPoints["D"]!![3], 45.degrees)
+    addWaypoint(navPoints["D"]!![3], 0.degrees)
     addWaypoint(navPoints["C"]!![6], 0.degrees)
-    addWaypoint(navPoints["D"]!![9], 315.degrees)
+    addWaypoint(navPoints["D"]!![9], 0.degrees)
     addWaypoint(navPoints["E"]!![10], 0.degrees)
-    addWaypoint(navPoints["D"]!![11], 90.degrees)
-    addWaypoint(navPoints["D"]!![9], 225.degrees)
-    addWaypoint(navPoints["E"]!![6], 180.degrees)
-    addWaypoint(navPoints["D"]!![3], 135.degrees)
+    addWaypoint(navPoints["D"]!![11], 0.degrees)
+    addWaypoint(navPoints["C"]!![10], 0.degrees)
+    addWaypoint(navPoints["D"]!![9], 0.degrees)
+    addWaypoint(navPoints["E"]!![6], 0.degrees)
+    addWaypoint(navPoints["D"]!![3], 0.degrees)
   }
 
 }
