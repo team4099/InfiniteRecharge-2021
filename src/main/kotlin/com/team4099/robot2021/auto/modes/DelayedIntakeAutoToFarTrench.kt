242293d66f6c15726729911a96972073c72ae734
sswadkar <saraanshwadkar@gmail.com>
2021-11-05 17:52:27 -0400

    intake drop down and intake roller set

--- src/main/kotlin/com/team4099/robot2021/auto/modes/DelayedIntakeAutoToFarTrench.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/modes/DelayedIntakeAutoToFarTrench.kt
@@ -9,29 +9,33 @@ import com.team4099.robot2021.subsystems.Drivetrain
 import com.team4099.robot2021.subsystems.Intake
 import com.team4099.robot2021.subsystems.Shooter
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 
-class TestAuto: ParallelCommandGroup() {
+class DelayedIntakeAutoToFarTrench: ParallelCommandGroup() {
 
   init {
     addRequirements(Drivetrain, Intake, Shooter)
 
     addCommands(
         ParallelCommandGroup(
-            SequentialCommandGroup(AutoDriveCommand(PathStore.toFarTrench), WaitCommand(0.5)),
-            SequentialCommandGroup(
-                //put intake down (idk the command for this)
-                WaitCommand(0.0) //distance travelling over robot speed * 0.75
-                //intake command vroom vroom (idk command for this)
-            ),
+          SequentialCommandGroup(AutoDriveCommand(PathStore.toFarTrench), WaitCommand(0.5)),
+          SequentialCommandGroup(
+            //put intake down
             IntakeCommand(
-                Constants.Intake.IntakeState.IN,
-                Constants.Intake.ArmPosition.OUT)
-        ),
-        AutoDriveCommand(PathStore.fromFarTrench),
-        ShootCommand()
+              Constants.Intake.IntakeState.IDLE,
+              Constants.Intake.ArmPosition.OUT
+              ),
+            WaitCommand(0.0), //to-do: distance travelling over robot speed * 0.75 (figure out how PIDs work first)
+            //
+            IntakeCommand(
+              Constants.Intake.IntakeState.IN,
+              Constants.Intake.ArmPosition.OUT
+              )
+            ),
+          AutoDriveCommand(PathStore.fromFarTrench),
+          ShootCommand()
+        )
     )
   }
 }
