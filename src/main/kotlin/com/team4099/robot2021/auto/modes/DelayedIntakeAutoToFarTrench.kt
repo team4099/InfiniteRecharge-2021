fe773fd6cc8a55bd5b661f4db1e8e3eb540ccd57
sswadkar <saraanshwadkar@gmail.com>
2021-11-06 21:18:35 -0400

    extend fixes

--- src/main/kotlin/com/team4099/robot2021/auto/modes/DelayedIntakeAutoToFarTrench.kt
+++ src/main/kotlin/com/team4099/robot2021/auto/modes/DelayedIntakeAutoToFarTrench.kt
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.WaitCommand
 
-class DelayedIntakeAutoToFarTrench: ParallelCommandGroup() {
+class DelayedIntakeAutoToFarTrench: SequentialCommandGroup() {
 
   init {
     addRequirements(Drivetrain, Intake, Shooter)
